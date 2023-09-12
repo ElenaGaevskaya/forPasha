4abf7bcaff5ad6d6b75b74232fb8f69c2dee5ad7
204540 <alenkapyh1@gmail.com>
2023-09-12 15:09:05 +0300

    hi

---
+++ src/lol.java
@@ -0,0 +1,10 @@
+public class lol extends Calc {
+    private String kek;
+    private String colors;
+    private int ages;
+    private float weights;
+    private float heights;
+    public lol(String name, String color, int age, float weight, float height) {
+        super(name, color, age, weight, height);
+    }
+}
